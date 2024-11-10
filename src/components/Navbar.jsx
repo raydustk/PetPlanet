c6a5af901e2a24b2c9d7a86f644cef0811fbc1f9
Valentina Alvarez de Lugo <141885784+raydustk@users.noreply.github.com>
2024-11-10 19:42:27 -0300

    h

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/profile">Profile</Link>
+            <Link to="/">
+        <img src="src/assets/New Project (12).png" style={{ height: '40px'}}/>
+      </Link>
+      <Link to="/" style={{}}>Home </Link>
+      <Link to="/login">Login </Link>
+      <Link to="/register">Register </Link>
+      <Link to="/profile">Profile </Link>
       <Link to="/create-post">Create Post</Link>
     </nav>
   );
